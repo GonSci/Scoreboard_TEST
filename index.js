2d18d2354cb6befbb06f0ef713447107fcec07de
GonSci <igonneski@gmail.com>
2024-10-12 10:43:51 +0800

    Added pending tasks and improved checklist

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const inputField = document.querySelector(".input-field textarea"),
     todoLists = document.querySelector(".todoLists"),
-    pendingNum = document.querySelector(".pending-nm"),
+    pendingNum = document.querySelector(".pending-num"),
     clearButton = document.querySelector(".clear-button");
 
+function allTasks() {
+    let tasks = document.querySelectorAll(".pending");
+    pendingNum.textContent = tasks.length === 0? "no" : tasks.length;
+    }
+
 inputField.addEventListener("keyup", (e) => {
+    let inputVal = inputField.value.trim();
 
 
 
-    if (e.key === "Enter") {
-        console.log("this is enter key")
+    if (e.key === "Enter" && inputVal.length > 0) {
+        console.log("valid")
+        let liTag =  `<li class="list pending" onclick="handleStatus(this)">
+            <input type="checkbox"/>
+            <span class="task">${inputVal}</span>
+            <i class="uil uil-trash"></i> `;
+        
+        todoLists.insertAdjacentHTML("beforeend", liTag);
+        inputField.value = ""; //removing value from input gield
+        allTasks();
+
     }
 });
+
+function handleStatus(e) {
+    const checkbox = e.querySelector("input");
+    checkbox.checked = checkbox.checked ? false : true;
+    e.classList.toggle("pending");
+    allTasks();
+
+}
 
 
